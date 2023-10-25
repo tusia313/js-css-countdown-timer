c56a583f955274a511290a7520fae950b1ddab81
Marta Łoskot <loskotmarta@gmail.com>
2023-10-25 15:23:02 +0200

    Add screenshots and some remarks.

--- js/app.js
+++ js/app.js
@@ -9,39 +9,43 @@ headingElement.innerText = timeLeft
 
 const time = setInterval(() => {
 
-        timeLeft--
-        // value = value - step
-        headingElement.textContent = timeLeft
-        sliderElement.style.width = (timeLeft/startCount) * 100 + '%'
+    timeLeft--
+    // value = value - step
+    headingElement.textContent = timeLeft
+    sliderElement.style.width = (timeLeft / startCount) * 100 + '%'
 
-     if (timeLeft === 0) {
+    if (timeLeft === 0) {
         clearInterval(time)
         headingElement.textContent = 'Congrats !'
         headingElement.style.color = 'white'
-        suprise()
+        surprise()
     }
 
 }, 1000)
 
 
-function suprise() {
+function surprise() {
     const colors = [
         'rgba(245,17,148, 0.5)',
         'rgba(245,107,59, 0.5)',
         'rgba(245,219,82, 0.5)',
-        'rgba(245,178,95, 0.5)',
+        'rgba(245, 178, 95, 0.5)',
+        'rgba(248, 23, 195, 0.5)',
+        'rgba(248, 23, 195, 0.5)',
+        'rgba(219, 207, 192, 0.7)',
     ]
 
-    for (let i=0; i<500; i++) {
+    for (let i = 0; i < 500; i++) {
         setTimeout(() => {
             const circleElement = document.createElement('div')
+            console.log(circleElement)
             circleElement.classList.add('circle')
-            circleElement.style.backgroundColor = colors[Math.floor(Math.random()) * colors.lenght] 
             circleElement.style.left = Math.floor(Math.random() * 100) + '%'
             circleElement.style.top = Math.floor(Math.random() * 100) + '%'
+            circleElement.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
             headingElement.append(circleElement)
-        }
-        , i * 5)
+        }, i * 5)
     }
 }
+
 
