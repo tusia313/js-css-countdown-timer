9551550f5e56a218e4b9408b6949517fc57ec5b9
Marta Łoskot <loskotmarta@gmail.com>
2023-10-25 12:22:57 +0200

    Add final countdown timer with stylished slider.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
 const headingElement = document.querySelector('#time-left')
-const sliderElement = document.querySelector('#slider')
+const sliderElement = document.querySelector('.fill')
 
 let timeLeft = 5
-let value = 100
+let startCount = timeLeft
+
 
 headingElement.innerText = timeLeft
 
 const time = setInterval(() => {
 
         timeLeft--
-        value = value - 20
+        // value = value - step
         headingElement.textContent = timeLeft
-        sliderElement.value = value
+        sliderElement.style.width = (timeLeft/startCount) * 100 + '%'
 
      if (timeLeft === 0) {
         clearInterval(time)
-        headingElement.textContent = 'Congrats!'
+        headingElement.textContent = 'Congrats !'
+        headingElement.style.color = 'white'
     }
 
 }, 1000)
