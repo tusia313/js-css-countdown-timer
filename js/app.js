1d43703d0161b19f453556d24797598b0e94180d
Marta Łoskot <loskotmarta@gmail.com>
2023-10-25 12:01:44 +0200

    Add sketch of slider in js

--- js/app.js
+++ js/app.js
@@ -2,20 +2,18 @@ const headingElement = document.querySelector('#time-left')
 const sliderElement = document.querySelector('#slider')
 
 let timeLeft = 5
-const range = 100/15
-console.log(range)
+let value = 100
+
 headingElement.innerText = timeLeft
 
 const time = setInterval(() => {
 
-    if (timeLeft > 0) {
         timeLeft--
-        let remain = 100 - range
+        value = value - 20
         headingElement.textContent = timeLeft
-        sliderElement.value = remain
-    }
+        sliderElement.value = value
 
-     else if (timeLeft = 0) {
+     if (timeLeft === 0) {
         clearInterval(time)
         headingElement.textContent = 'Congrats!'
     }
